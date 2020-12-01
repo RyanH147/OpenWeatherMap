3ca96f90954f783ca600dff9fa5a8f10d5ed3241
Ryan Hagood <ryanh147@gmail.com>
2020-11-30 21:03:25 -0600

    Committing OpenWeatherMap Project Complete

---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace OpenWeatherMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("apiKey.json");
+            string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+            Console.Write("Please enter your Zip Code: ");
+            string zipCode = Console.ReadLine();
+            Console.Write("Please enter your Country Code: ");
+            string countryCode = Console.ReadLine().ToLower();
+
+            string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},{countryCode}&appid={APIkey}";
+
+            Console.WriteLine(WeatherAPI.GetTemp(apiCall));
+        }
+    }
+}
